e58406616b4fe4d3e7543258066a8ce612e68d46
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2024-12-05 17:09:02 +0100

    wip

--- ui/src/pages/GraphPage.vue
+++ ui/src/pages/GraphPage.vue
@@ -1,47 +1,43 @@
 <script setup lang="ts">
-import { GraphMaker, GraphMakerSettings } from "@milaboratories/graph-maker";
+import { GraphMaker, GraphMakerProps } from "@milaboratories/graph-maker";
 import '@milaboratories/graph-maker/styles';
 import { useApp } from "../app";
 
 const app = useApp();
 
-const settings = {
-  chartType: 'scatterplot',
-  template: 'dots',
-  title: 'Differentially expressed genes',
-  defaultOptions: [
-    {
-      inputName: 'x',
-      selectedSource: {
-        kind: 'PColumn',
-        name: "pl7.app/rna-seq/log2foldchange",
-        valueType: "Double",
-        axesSpec: [
-          {
-            name: "pl7.app/Geneid",
-            type: "String"
-          }
-        ]
-      }
-    },
-    {
-      inputName: 'y',
-      selectedSource: {
-        kind: 'PColumn',
-        name: "pl7.app/rna-seq/minlog10padj",
-        valueType: "Double",
-        axesSpec: [
-          {
-            name: "pl7.app/Geneid",
-            type: "String"
-          }
-        ]
-      }
+const defaultOptions: GraphMakerProps['defaultOptions'] = [
+  {
+    inputName: 'x',
+    selectedSource: {
+      kind: 'PColumn',
+      name: "pl7.app/rna-seq/log2foldchange",
+      valueType: "Double",
+      axesSpec: [
+        {
+          name: "pl7.app/Geneid",
+          type: "String"
+        }
+      ]
     }
-  ]
-} as GraphMakerSettings;
+  },
+  {
+    inputName: 'y',
+    selectedSource: {
+      kind: 'PColumn',
+      name: "pl7.app/rna-seq/minlog10padj",
+      valueType: "Double",
+      axesSpec: [
+        {
+          name: "pl7.app/Geneid",
+          type: "String"
+        }
+      ]
+    }
+  }
+];
 </script>
 
 <template>
-  <graph-maker :pFrame="app.model.outputs.topTablePf" v-model="settings" />
+  <GraphMaker chartType="scatterplot" :p-frame="app.model.outputs.topTablePf" v-model="app.model.ui.graphState"
+    :defaultOptions="defaultOptions" />
 </template>
